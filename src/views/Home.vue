c63044d24973de27d3a09bdb960b8f4b1893c843
wanghao <281463547@qq.com>
2022-10-20 23:38:40 +0800

    退出功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <h2>主页</h2>
+  <div>
+    <h2>主页</h2>
+    <el-button @click.native.prevent="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
+import {removeToken} from "@/utils/auth";
+import ElementUI from "element-ui";
+
 export default {
-  name: "MyHome"
+  name: "MyHome",
+  methods: {
+    logout() {
+      this.$request.get('http://localhost:8088/auth/logout').then(res => {
+        if (res.code === 200) {
+          removeToken()
+          this.$router.replace('/')
+          ElementUI.Message.success(res.message)
+        }
+      })
+    }
+  }
 }
 </script>
 
