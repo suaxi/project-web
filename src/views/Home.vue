e83911cc2d16ba881bd37fbb0cc5eb9e6ae4bdbb
wanghao <281463547@qq.com>
2022-10-22 22:23:49 +0800

    主页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,82 @@
 <template>
-  <div>
-    <h2>主页</h2>
-    <el-button @click.native.prevent="getUserInfo">获取用户信息</el-button>
-    <el-button @click.native.prevent="logout">退出</el-button>
+  <div style="height: 100%">
+    <el-container style="height: 100%">
+      <el-aside width="200px">
+        <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header style="text-align: right">
+          <el-dropdown>
+              <i class="el-icon-setting" style="margin: 15px">
+                孙笑川
+              </i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+              <el-dropdown-item @click.native.prevent="logout">退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-header>
+        <el-main>
+          <el-table
+              ref="singleTable"
+              :data="tableData"
+              highlight-current-row
+              style="width: 100%">
+            <el-table-column
+                type="index"
+                width="50">
+            </el-table-column>
+            <el-table-column
+                property="date"
+                label="日期"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                property="name"
+                label="姓名"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                property="address"
+                label="地址">
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -12,6 +86,27 @@ import ElementUI from "element-ui";
 
 export default {
   name: "MyHome",
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
   methods: {
     logout() {
       this.$request.get('/api/auth/logout').then(res => {
