168a17d493ff8816f20856f2a502e4f1cec7ed41
suaxi <281463547@qq.com>
2025-09-28 21:32:19 +0800

    tab tool

--- project-web-vue3/src/main.js
+++ project-web-vue3/src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/styles/index.scss'
+import tab from '@/utils/tab'
 
 // svg
 import 'virtual:svg-icons-register'
@@ -19,5 +20,8 @@ app.use(store)
 app.use(ElementPlus)
 app.use(elementIcons)
 app.component('svg-icon', SvgIcon)
+
+// tab
+app.config.globalProperties.$tab = tab
 
 app.mount('#app')
