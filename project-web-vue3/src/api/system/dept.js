36aad1dc84293d4cbbd12c278bbe32daf5e64315
suaxi <281463547@qq.com>
2025-09-29 23:39:22 +0800

    用户管理

---
+++ project-web-vue3/src/api/system/dept.js
@@ -0,0 +1,57 @@
+import request from '@/utils/request'
+import qs from 'qs'
+
+export function add(data) {
+  return request({
+    url: '/dept',
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/dept',
+    method: 'put',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: '/dept',
+    method: 'delete',
+    data: ids
+  })
+}
+
+export function childList(params) {
+  return request({
+    url: '/dept/child-list',
+    method: 'get',
+    params
+  })
+}
+
+export function superiorList(ids) {
+  const data = ids.length || ids.length === 0 ? ids : Array.of(ids)
+  return request({
+    url: '/dept/superior-list',
+    method: 'post',
+    data
+  })
+}
+
+export function getDept(id) {
+  return request({
+    url: `/dept/id/${id}`,
+    method: 'get'
+  })
+}
+
+export function page(params) {
+  return request({
+    url: '/dept/page?' + qs.stringify(params, { indices: false }),
+    method: 'get'
+  })
+}
